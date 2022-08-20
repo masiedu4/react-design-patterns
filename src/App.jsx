908127a48b069350bdb61d8f97777e8b7ac1be9f
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-20 16:10:39 +0000

    branch setup

--- src/App.jsx
+++ src/App.jsx
@@ -10,45 +10,16 @@
     * - Author          : Michael
     * - Modification    : 
 **/
-import { useState } from 'react'
-import styled from 'styled-components'
+
+
 import './App.css'
-import SplitScreen from './SplitScreen'
 
-
-// styles for left side of the screen
-const Container = styled.div`
- background-color:red ;
-`
-
-// styles for the right side of the screen
-const Container2 = styled.div`
-background-color:blue;
-`
-// component for left side of the screen
-const LeftHandScreen = () => {
-  return (
-    <Container>
-      <h1>Left Hand Screen</h1>
-    </Container>
-  )
-}
-
-// component for the right side of the screen
-const RightHandScreen = () => {
-  return (
-    <Container2>
-      <h1>Right Hand Screen</h1>
-    </Container2>
-  )
-}
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App">
-      <SplitScreen left={LeftHandScreen} right={RightHandScreen} />
-    </div>
+    <>
+    </>
   )
 }
 
