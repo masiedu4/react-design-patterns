d80889827463bef0000f855a0621e0ec17b2de22
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-20 17:10:25 +0000

    added numbered list

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,22 @@ import { people, products } from "./data"
 import SmallList from "./components/people/SmallList";
 import LargeList from "./components/people/LargeList";
 import GeneralList from "./components/GeneralList";
+import SmallProductList from "./components/products/SmallProductList";
+import LargeProductList from "./components/products/LargeProductList";
+import NumberedList from "./components/NumberedList";
 
 function App() {
   console.log(people, products);
 
   return (
     <>
+      {/* using GeneralList for people components */}
       <GeneralList items={people} resourceName="person" itemComponent={SmallList} />
       <GeneralList items={people} resourceName="person" itemComponent={LargeList} />
+      {/* using NumberedList for products components */}
+      <NumberedList items={products} resourceName="product" itemComponent={SmallProductList} />
+      <NumberedList items={products} resourceName="product" itemComponent={LargeProductList} />
+
     </>
   )
 }
