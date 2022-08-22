c7eb125bc922b6f451343d5bbdc0cd81f7dfe010
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-22 14:14:07 +0000

    react-modal best practices

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
 /**
-    * @description      : 
-    * @author           : Michael
-    * @group            : 
-    * @created          : 16/08/2022 - 17:18:36
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 16/08/2022
-    * - Author          : Michael
-    * - Modification    : 
-**/
+ * @description      :
+ * @author           : Michael
+ * @group            :
+ * @created          : 16/08/2022 - 17:18:36
+ *
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 16/08/2022
+ * - Author          : Michael
+ * - Modification    :
+ **/
 
+import { people, products } from "./data";
 
-import { people, products } from "./data"
-import SmallList from "./components/people/SmallList";
-import LargeList from "./components/people/LargeList";
-import GeneralList from "./components/GeneralList";
-import SmallProductList from "./components/products/SmallProductList";
-import LargeProductList from "./components/products/LargeProductList";
-import NumberedList from "./components/NumberedList";
-
+import LargeProductDetail from "./components/products/LargeProductDetail";
+import Modal from "./components/Modal";
 function App() {
   console.log(people, products);
 
   return (
     <>
-      {/* using GeneralList for people components */}
-      <GeneralList items={people} resourceName="person" itemComponent={SmallList} />
-      <GeneralList items={people} resourceName="person" itemComponent={LargeList} />
-      {/* using NumberedList for products components */}
-      <NumberedList items={products} resourceName="product" itemComponent={SmallProductList} />
-      <NumberedList items={products} resourceName="product" itemComponent={LargeProductList} />
-
+      <Modal>
+        <LargeProductDetail product={products[0]} />
+      </Modal>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/products/LargeProductDetail.jsx
@@ -0,0 +1,28 @@
+/**
+ * @description      :
+ * @author           : Michael
+ * @group            :
+ * @created          : 20/08/2022 - 17:02:48
+ *
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 20/08/2022
+ * - Author          : Michael
+ * - Modification    :
+ **/
+import React from "react";
+
+const LargeProductDetail = ({ product }) => {
+  const { name, price, description, rating } = product;
+
+  return (
+    <>
+      <h3> {name}</h3>
+      <h3>{price}</h3>
+      <p> {description} </p>
+      <p> {rating} </p>
+    </>
+  );
+};
+
+export default LargeProductDetail;
