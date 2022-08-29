f9cd025e1914d131fd52fc71063a128f6359a227
Michael Asiedu <michaelasiedu148@gmail.com>
2022-08-29 00:28:45 +0000

    uncontrolled data

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,21 @@
 /**
-    * @description      : 
-    * @author           : Michael
-    * @group            : 
-    * @created          : 16/08/2022 - 17:18:36
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 16/08/2022
-    * - Author          : Michael
-    * - Modification    : 
-**/
-
-import styled from 'styled-components'
-import './App.css'
-import SplitScreen from './SplitScreen'
-
-
-// styles for left side of the screen
-const Container = styled.div`
- background-color:red ;
-`
-
-// styles for the right side of the screen
-const Container2 = styled.div`
-background-color:blue;
-`
-// component for left side of the screen
-const LeftHandScreen = () => {
-  return (
-    <Container>
-      <h1>Left Hand Screen</h1>
-    </Container>
-  )
-}
-
-// component for the right side of the screen
-const RightHandScreen = () => {
-  return (
-    <Container2>
-      <h1>Right Hand Screen</h1>
-    </Container2>
-  )
-}
+ * @description      :
+ * @author           : Michael
+ * @group            :
+ * @created          : 29/08/2022 - 00:26:20
+ *
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 29/08/2022
+ * - Author          : Michael
+ * - Modification    :
+ **/
 function App() {
-
-
   return (
     <div className="App">
-      <SplitScreen left={LeftHandScreen} right={RightHandScreen} />
+      <h1>Hello</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
